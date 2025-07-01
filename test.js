c95f74ff947dcb144a9391cf6e3884b3cd07404a
yeonjae <yeonjaecho@dsm.hs.kr>
2025-07-01 11:09:56 +0900

    test

---
+++ test.js
@@ -0,0 +1,5 @@
+console.log(1); // 콘솔창에 로그 메시지를 보여줍니다.
+console.error("error"); // 콘솔창에 에러 메시지를 보여줍니다.
+console.warn("warn"); // 콘솔창에 경고 메시지를 보여줍니다.
+console.info('정보'); // 콘솔창에 정보 메시지를 보여줍니다.
+console.table("01001"); // 콘솔창에 데이터를 테이블 형태로 제공합니다.
